deac3a524c97d629bc185bcb45142aa24041d5ee
tamaslin-creator <tamas.lin@gmail.com>
2026-08-24 21:21:52 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ielts-learning-stable-v9';
+const CACHE_NAME = 'ielts-learning-stable-v10';
 const STATIC_ASSETS = ['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   self.skipWaiting();
