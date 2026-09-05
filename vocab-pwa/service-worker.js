3bd9f0195bd9d69005b31f1cdd7829c5f58e85c7
tamaslin-creator <tamas.lin@gmail.com>
2026-09-05 18:25:18 +0800

    新增單字記憶卡小工具

---
+++ vocab-pwa/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'vocab-flashcards-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).then((response) => {
+          if (response && response.status === 200 && event.request.method === 'GET') {
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseClone));
+          }
+          return response;
+        }).catch(() => cached)
+      );
+    })
+  );
+});
